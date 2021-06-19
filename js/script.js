17b9fa1fd1043038fc6cf21f67486ecc08b99f79
Catherine <katjatolok3@gmai.com>
2021-06-19 19:35:06 +0200

    doing section gallery

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(".sliders").slick({
+  dots: true,
+  Infinity: true,
+  fade: true,
+  speed: 300,
+  slidesToScroll: 1,
+});
+
+// $(".cards").slick({
+//   dots: true,
+//   Infinity: true,
+//   fade: true,
+//   speed: 300,
+//   slidesToScroll: 1,
+// });
