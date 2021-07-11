101b44c2df5de0aeb737942e62c4d47565082360
Catherine <katjatolok3@gmai.com>
2021-07-11 14:00:31 +0200

    done section lates news added slider

--- js/script.js
+++ js/script.js
@@ -3,13 +3,21 @@ $(".sliders").slick({
   Infinity: true,
   fade: true,
   speed: 300,
-  slidesToScroll: 1,
 });
 
-// $(".cards").slick({
-//   dots: true,
-//   Infinity: true,
-//   fade: true,
-//   speed: 300,
-//   slidesToScroll: 1,
-// });
+$(document).ready(function () {
+  $(".cards").slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    speed: 2000,
+    easing: "ease-out", //type of animation
+    Infinity: true, //показывает будет-ли слайдер бесконечным
+    autoplay: true, //слайды автомат будут пролистываться
+    autoplaySpeed: 800, //задает скорость пролистывания слайдов автоматически
+    pauseOnFocus: true, //слайдер будет активен если его перематывать мышкой\дергать
+  });
+});
+//pauseOnHover: true, //при наведении на всю область слайдера он будет останавливатся
+//pauseOnDotsHover: true, //пауза при наведении на точки
